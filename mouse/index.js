70fcd50d5917d2cb083260eb753b17ff81afc475
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-01-19 17:32:18 +0530

    new commit

---
+++ mouse/index.js
@@ -0,0 +1,125 @@
+
+const btn = document.querySelector('button')
+// console.log(btn);
+
+/* 
+btn.onclick = function(){
+    console.log('button clicked');
+    
+}
+*/
+
+
+// onmouseenter
+// onmouseover
+// onmouseleave
+// onmouseout
+// onmousemove
+// onmouseup
+// onmousedown
+
+
+/* 
+
+btn.addEventListener('mouseover',function(){
+    console.log('button clicked');
+    
+}, false)
+
+*/
+
+
+
+
+/*
+
+btn.addEventListener('mouseenter',function(){
+    console.log('button clicked');
+    
+}, false)
+
+*/
+
+
+
+
+
+/*
+
+btn.addEventListener('mouseleave',function(){
+    console.log('button clicked');
+    
+}, false)
+
+*/
+
+
+
+
+
+/*
+
+btn.addEventListener('mouseout',function(){
+    console.log('button clicked');
+    
+}, false)
+
+*/
+
+
+
+
+/*
+
+btn.addEventListener('mousemove',function(){
+    console.log('button clicked');
+    
+}, false)
+
+*/
+
+
+
+
+/*
+
+btn.addEventListener('mouseup',function(){
+    console.log('button clicked');
+    
+}, false)
+
+*/
+
+
+
+/*
+
+btn.addEventListener('mousedown',function(){
+    console.log('button clicked');
+    
+}, false)
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
