1a35a5faa4089b0b4c2320be8b9e0e43b91a4e35
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-01-19 17:32:03 +0530

    new commit

---
+++ dblclick/index.js
@@ -0,0 +1,18 @@
+
+const btn = document.querySelector('button')
+// console.log(btn);
+
+/* 
+btn.onclick = function(){
+    console.log('button clicked');
+    
+}
+*/
+
+
+btn.addEventListener('dblclick',function(){
+    console.log('button clicked');
+    
+}, false)
+
+
