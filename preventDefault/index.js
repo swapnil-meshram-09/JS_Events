048f3926de27a3fff218c3b57ba11f5d9ff02926
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-01-19 18:38:14 +0530

    Remove

---
+++ preventDefault/index.js
@@ -0,0 +1,24 @@
+
+const form = document.querySelector('form')
+// console.log(form);
+
+form.addEventListener('submit', (e) =>{
+    console.log('form');
+
+    e.preventDefault()
+    
+}, false)
+
+/*
+
+const btn = document.querySelector('button')
+// console.log(form);
+
+btn.addEventListener('click', (e) =>{
+    console.log('button clicked');
+
+    e.preventDefault()
+    
+}, false)
+
+*/
