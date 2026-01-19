3557568ac8343de8cc8c89e448b95d40b2b0c3f1
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-01-19 17:31:46 +0530

    new commit

---
+++ click/index.js
@@ -0,0 +1,21 @@
+
+const btn = document.querySelector('button')
+// console.log(btn);
+
+/* 
+btn.onclick = function(){
+    console.log('button clicked');
+    
+}
+*/
+
+
+btn.addEventListener('click',function(){
+    console.log('button clicked');
+    
+},false)
+
+
+
+
+
